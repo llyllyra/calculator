42c1f8f4f76e5d1da96f2a58fa7c6f746cd9ee88
llyllyra <llyllyrallylly@gmail.com>
2022-06-03 14:31:02 +0200

    creation calculator

---
+++ script.js
@@ -0,0 +1,47 @@
+
+let buttonCalcValue = [
+    "(",
+    ")",
+    "%",
+    "AC",
+    "7",
+    "8",
+    "9",
+    "/",
+    "4",
+    "5",
+    "6",
+    "X",
+    "1",
+    "2",
+    "3",
+    "-",
+    "0",
+    ".",
+    "=",
+    "+"
+]
+
+let main = document.querySelector("main")
+
+
+let number = buttonCalcValue.length
+
+let affichage = () =>{
+    let black = document.createElement("div");
+    black.classList.add("black");
+    main.appendChild(black)
+    for(i = 0; i < number; i++){
+        console.log(buttonCalcValue[i]);
+        let buttonDiv = document.createElement("div");
+        buttonDiv.innerHTML= buttonCalcValue[i];
+        buttonDiv.classList.add("button");
+        main.appendChild(buttonDiv);
+        
+    }
+}
+
+
+
+
+affichage();
