9fc64829bac55cf758dc01545335a1a9914cb04d
llyllyra <llyllyrallylly@gmail.com>
2022-06-13 10:29:29 +0200

    add

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-
 let buttonCalcValue = [
     "(",
     ")",
     "%",
     "AC",
-    "7",
-    "8",
-    "9",
+    7,
+    8,
+    9,
     "/",
-    "4",
-    "5",
-    "6",
+    4,
+    5,
+    6,
     "X",
-    "1",
-    "2",
-    "3",
+    1,
+    2,
+    3,
     "-",
-    "0",
+    0,
     ".",
     "=",
     "+"
@@ -24,30 +23,52 @@ let buttonCalcValue = [
 
 let main = document.querySelector("main")
 
-
 let number = buttonCalcValue.length
 
 let affichage = () =>{
     let black = document.createElement("div");
     black.classList.add("black");
     main.appendChild(black)
+
     for(i = 0; i < number; i++){
-        console.log(buttonCalcValue[i]);
         let buttonDiv = document.createElement("button");
         buttonDiv.innerHTML= buttonCalcValue[i];
         buttonDiv.classList.add("button");
+        buttonDiv.setAttribute('class', "buttonCal")
         if(buttonCalcValue[i] != "="){
             buttonDiv.setAttribute('id', buttonCalcValue[i])
+        
         }else{
             buttonDiv.setAttribute('id', "equals")
         }
-        main.appendChild(buttonDiv);
-        
-    }
-    
+        main.appendChild(buttonDiv);   
+    }   
 }
-
-
-
-
 affichage();
+let buttons = document.querySelectorAll(".buttonCal");
+let results = document.querySelector(".black")  
+let equal = document.getElementById("equals");
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if (e.target.id != "equals"){
+            results.textContent += e.target.id;
+        }
+      
+    });
+  });
+ 
+  let clear = document.getElementById("AC")
+  clear.addEventListener("click", ()=> {
+    results.textContent = ""
+  })
+
+  function computeResult(str) {
+    return Function("return " + str)();
+  }
+  
+  equal.addEventListener("click", () => {
+    results.textContent = computeResult(results.textContent);
+  });
+  
+
+
